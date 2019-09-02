4226ea1bd190809d70098f592601eb440f691847
Sabrina Xia <xrc68@berkeley.edu>
2019-09-02 02:48:19 -0700

    With Year.java modified

--- lab1/Year.java
+++ lab1/Year.java
@@ -1,11 +1,16 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Sabrina Xia
  */
 public class Year {
 
     /** Return true iff YEAR is a leap year.  */
     static boolean isLeapYear(int year) {
-        return true;    // TODO: YOUR CODE HERE
+      if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))) {
+        return true;
+      }
+      else {
+        return false;
+      }
     }
 
     /** Print whether YEAR is a a leap year on System.out. */
@@ -35,4 +40,3 @@ public class Year {
         }
     }
 }
-
