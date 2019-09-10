efd0898d9725f0e9d8b83e45d9dde8c751f97ba1
Sabrina Xia <xrc68@berkeley.edu>
2019-09-09 21:45:04 -0700

    compound interest done

--- hw1/CompoundInterest/CompoundInterest.java
+++ hw1/CompoundInterest/CompoundInterest.java
@@ -10,7 +10,7 @@ public class CompoundInterest {
      *  should be 1. Throughout the assignment it is OK to assume that
      *  TARGETYEAR is >= THIS_YEAR. */
     static int numYears(int targetYear) {
-        return 0;
+        return targetYear - THIS_YEAR;
     }
 
     /** Suppose we have an asset worth PRESENTVALUE that appreciates
@@ -22,7 +22,11 @@ public class CompoundInterest {
      *  then the futureValue will be 10*1.12*1.12 = 12.544. */
     static double futureValue(double presentValue, double rate,
                               int targetYear) {
-        return 0;
+        int power = numYears(targetYear);
+        double rateForCompute = 1 + (rate/100);
+        double totalRate = Math.pow(rateForCompute, power);
+        double result = presentValue * totalRate;
+        return result;
     }
 
     /** Returns returns the value, in THIS_YEAR dollars, of an asset
@@ -36,7 +40,12 @@ public class CompoundInterest {
      *  2019 dollars, we get 12.544 * 0.97 * 0.97 = 11.8026496 dollars. */
     static double futureValueReal(double presentValue, double rate,
                                   int targetYear, double inflationRate) {
-        return 0;
+        int power = numYears(targetYear);
+        double rateForCompute = 1 + (rate/100);
+        double IRforCompute = 1 - (inflationRate/100);
+        double totalRate = Math.pow(rateForCompute * IRforCompute, power);
+        double result = presentValue * totalRate;
+        return result;
     }
 
     /** Suppose you invest PERYEAR dollars at the end of every year until
@@ -47,7 +56,15 @@ public class CompoundInterest {
      *  then the result will be 5000*1.1*1.1 + 5000*1.1 + 5000 =
      *  16550. */
     static double totalSavings(double perYear, int targetYear, double rate) {
-        return 0;
+        int years = numYears(targetYear);
+        double result = 0;
+        double rateForCompute = 1 + (rate/100);
+        for (; years >= 0; years -= 1) {
+            double rateThisYear = Math.pow(rateForCompute, years);
+            double thisYear = perYear * rateThisYear;
+            result += thisYear;
+        }
+        return result;
     }
 
     /** Returns totalSavings(PERYEAR, TARGETYEAR, RATE) converted to
@@ -55,8 +72,15 @@ public class CompoundInterest {
      *  INFLATIONRATE. */
     static double totalSavingsReal(double perYear, int targetYear, double rate,
                                double inflationRate) {
-        return 0;
-
+        int years = numYears(targetYear);
+        double result = 0;
+        double totalRate = (1 + (rate/100)) * (1 - (inflationRate/100));
+        for (; years >= 0; years -= 1) {
+            double rateThisYear = Math.pow(totalRate, years);
+            double thisYear = perYear * rateThisYear;
+            result += thisYear;
+        }
+        return result;
     }
 
     /** Prints out the future inflation-adjusted value of a dollar in
@@ -65,8 +89,8 @@ public class CompoundInterest {
      *  INFLATIONRATE. */
     static void printDollarFV(int targetYear, double returnRate,
                               double inflationRate) {
-        double nominalDollarValue = 0; // replace 0 with your code
-        double realDollarValue = 0;    // replace 0 with your code
+        double nominalDollarValue = futureValue(1, returnRate, targetYear); // replace 0 with your code
+        double realDollarValue = futureValueReal(1, returnRate, targetYear, inflationRate);    // replace 0 with your code
 
         // Do not change anything in this method below this line
         String dollarSummary =
--- hw1/CompoundInterest/CompoundInterestTest.java
+++ hw1/CompoundInterest/CompoundInterestTest.java
@@ -8,27 +8,43 @@ public class CompoundInterestTest {
         /** Sample assert statement for comparing integers.
 
         assertEquals(0, 0); */
+
+        assertEquals(6, CompoundInterest.numYears(2025));
+        assertEquals(31, CompoundInterest.numYears(2050));
     }
 
     @Test
     public void testFutureValue() {
         double tolerance = 0.01;
+        assertEquals(12.54, CompoundInterest.futureValue(10, 12, 2021), tolerance);
+        assertEquals(10, CompoundInterest.futureValue(10, 0, 2088), tolerance);
+        assertEquals(34.98, CompoundInterest.futureValue(20, 15, 2023), tolerance);
+        assertEquals(8.1, CompoundInterest.futureValue(10, -10, 2021), tolerance);
+
     }
 
     @Test
     public void testFutureValueReal() {
         double tolerance = 0.01;
+        assertEquals(11.80, CompoundInterest.futureValueReal(10, 12, 2021, 3), tolerance);
+        assertEquals(7.621, CompoundInterest.futureValueReal(10, -10, 2021, 3),tolerance);
+        assertEquals(13.307, CompoundInterest.futureValueReal(10, 12, 2021, -3), tolerance);
     }
 
 
     @Test
     public void testTotalSavings() {
         double tolerance = 0.01;
+        assertEquals(16550, CompoundInterest.totalSavings(5000, 2021, 10), tolerance);
+        assertEquals(2710, CompoundInterest.totalSavings(1000, 2021, -10), tolerance);
     }
 
     @Test
     public void testTotalSavingsReal() {
         double tolerance = 0.01;
+        assertEquals(16027.445, CompoundInterest.totalSavingsReal(5000, 2021, 10, 3), tolerance);
+        assertEquals(2635.129, CompoundInterest.totalSavingsReal(1000, 2021, -10, 3), tolerance);
+        assertEquals(17083.445, CompoundInterest.totalSavingsReal(5000, 2021, 10, -3), tolerance);
     }
 
 
