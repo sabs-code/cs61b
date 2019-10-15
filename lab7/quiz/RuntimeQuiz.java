97961054db7598ad5a772c75120967293e13527a
Sabrina Xia <xrc68@berkeley.edu>
2019-10-14 19:45:22 -0700

    lab7 fixed

--- lab7/quiz/RuntimeQuiz.java
+++ lab7/quiz/RuntimeQuiz.java
@@ -19,7 +19,7 @@ public class RuntimeQuiz {
      * big omega, big O, and big theta runtimes in terms on N where N is 
      * the length of the input array.
      */
-    public static Runtime f1_omega_runtime = Runtime.LINEAR;
+    public static Runtime f1_omega_runtime = Runtime.CONSTANT;
     public static Runtime f1_o_runtime = Runtime.QUADRATIC;
     public static Runtime f1_theta_runtime = Runtime.QUADRATIC;
     public void f1(int[] array) {
@@ -30,7 +30,7 @@ public class RuntimeQuiz {
         }
     }
 
-    public static Runtime f2_omega_runtime = Runtime.LINEAR;
+    public static Runtime f2_omega_runtime = Runtime.CONSTANT;
     public static Runtime f2_o_runtime = Runtime.CUBIC;
     public static Runtime f2_theta_runtime = Runtime.CUBIC;
     public int f2(int n) {
@@ -39,7 +39,7 @@ public class RuntimeQuiz {
         return n + n * f2(n - 1) + n * n * f2(1);
     }
 
-    public static Runtime f3_omega_runtime = Runtime.CONSTANT;
+    public static Runtime f3_omega_runtime = Runtime.LOG_N;
     public static Runtime f3_o_runtime = Runtime.LOG_N;
     public static Runtime f3_theta_runtime = Runtime.LOG_N;
     /* When f3 is first called, start will be 0 and end will be the length of the array - 1 */
