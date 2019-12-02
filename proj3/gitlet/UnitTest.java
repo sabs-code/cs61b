ba02c41cdb34022ff0d59517cc8c2cfbf10b034b
Paul Hilfinger <Hilfinger@cs.berkeley.edu>
2019-12-01 20:33:22 -0800

    Update proj3

--- proj3/gitlet/UnitTest.java
+++ proj3/gitlet/UnitTest.java
@@ -12,7 +12,7 @@ public class UnitTest {
     /** Run the JUnit tests in the loa package. Add xxxTest.class entries to
      *  the arguments of runClasses to run other JUnit tests. */
     public static void main(String[] ignored) {
-        textui.runClasses(UnitTest.class);
+        System.exit(textui.runClasses(UnitTest.class));
     }
 
     /** A dummy test to avoid complaint. */
